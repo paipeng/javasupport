6840ad44cf1e3df8b5dce332e8f81a3498f924a2
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-11 05:49:05 +0000

    [maven-release-plugin]  copy for tag javasupport-0.0.1

---
+++ tags/javasupport-0.0.1/maven-support/bin/create-javaapp.sh
@@ -0,0 +1,12 @@
+if [[ $# < 2 ]]; then
+	echo "create-javaapp.sh <groupId> <artifactId>"
+	exit 1
+fi
+
+mvn archetype:create \
+  -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal \
+  -DarchetypeGroupId=javasupport.maven \
+  -DarchetypeArtifactId=maven-archetype-javaapp \
+  -DarchetypeVersion=0.0.1 \
+  -DgroupId=$1 \
+  -DartifactId=$2
---
+++ tags/javasupport-0.0.1/maven-support/maven-archetype-springwebapp/src/main/resources/archetype-resources/src/main/java/web/SystemInfoController.java
@@ -0,0 +1,52 @@
+package ${groupId}.web;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.time.DurationFormatUtils;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class SystemInfoController extends AbstractController {
+
+    private int MB = 1024 * 1024;
+
+    private Properties applicationProperties;
+
+    public void setApplicationProperties(Properties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+    
+    public ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        ModelAndView mav = new ModelAndView("systeminfo");
+        mav.addObject("sysprops", new TreeMap<String, String>((Map) System.getProperties()));
+        mav.addObject("sysenv", new TreeMap<String, String>((Map) System.getenv()));
+
+        //Calculate app uptime duration
+        Date appStartTime = (Date) getServletContext().getAttribute("appStartTime");
+        long durationMillis = new Date().getTime() - appStartTime.getTime();
+        String duration = DurationFormatUtils.formatDurationWords(durationMillis, true, false);
+        mav.addObject("appUptime", duration);
+
+        //Calculate system resources.
+        Runtime runtime = Runtime.getRuntime();
+        long totalMem = runtime.totalMemory() / MB;
+        long freeMem = runtime.freeMemory() / MB;
+        long avaCPU = runtime.availableProcessors();
+        mav.addObject("sysres", "CPU: " + avaCPU + ", MemoryUsed: " + (totalMem - freeMem) + "M/" + totalMem + "M");
+        
+        if(applicationProperties== null){
+            applicationProperties = new Properties();
+        }
+        mav.addObject("applicationProps", applicationProperties);
+        
+        return mav;
+    }
+}
