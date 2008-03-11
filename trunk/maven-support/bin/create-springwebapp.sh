c2b36b63b10466884725ba8853b2cccd1334c463
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-11 00:32:18 +0000

    set to 0.0.1

--- trunk/maven-support/bin/create-springwebapp.sh
+++ trunk/maven-support/bin/create-springwebapp.sh
@@ -7,6 +7,6 @@ mvn archetype:create \
   -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal \
   -DarchetypeGroupId=javasupport.maven \
   -DarchetypeArtifactId=maven-archetype-springwebapp \
-  -DarchetypeVersion=0.0.1-SNAPSHOT \
+  -DarchetypeVersion=0.0.1 \
   -DgroupId=$1 \
   -DartifactId=$2
