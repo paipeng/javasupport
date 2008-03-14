10d4fbb5866182da28e0f8c05580f57e9206a06b
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-14 00:26:15 +0000

    prepared script for release.

--- trunk/maven-support/bin/create-scalaapp.sh
+++ trunk/maven-support/bin/create-scalaapp.sh
@@ -7,6 +7,6 @@ mvn org.apache.maven.plugins:maven-archetype-plugin:1.0-alpha-7:create \
   -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal \
   -DarchetypeGroupId=javasupport.maven \
   -DarchetypeArtifactId=maven-archetype-scalaapp \
-  -DarchetypeVersion=0.0.2-SNAPSHOT \
+  -DarchetypeVersion=0.0.2 \
   -DgroupId=$1 \
   -DartifactId=$2
--- trunk/maven-support/bin/create-webapp.bat
+++ trunk/maven-support/bin/create-webapp.bat
@@ -1,1 +1,1 @@
-mvn archetype:create -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal -DarchetypeGroupId=javasupport.maven -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeVersion=0.0.2-SNAPSHOT -DgroupId=%1 -DartifactId=%2
+mvn archetype:create -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal -DarchetypeGroupId=javasupport.maven -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeVersion=0.0.2 -DgroupId=%1 -DartifactId=%2
