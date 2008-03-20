355dfc4d47f5e922e19ac166423312536321fb10
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-20 20:33:00 +0000

    corrected template path.

--- trunk/template-generator/GenerateSpringMvc.groovy
+++ trunk/template-generator/GenerateSpringMvc.groovy
@@ -30,8 +30,8 @@ class GenerateSpringMvc {
 		main.data.scaffoldType = args[0]
 		main.data.className = args[1]
 		main.data.fields = args[2..-1]
-		//main.data.scriptName = System.properties['script.name']
-		main.data.scriptName = "/Users/thebugslayer/projects/java/javasupport/template-generator/GenerateSpringMvc.groovy"
+		main.data.scriptName = System.properties['script.name']
+		//main.data.scriptName = System.properties['user.home'] + "/projects/java/javasupport/template-generator/GenerateSpringMvc.groovy"
 		main.run()
 	}
 	
