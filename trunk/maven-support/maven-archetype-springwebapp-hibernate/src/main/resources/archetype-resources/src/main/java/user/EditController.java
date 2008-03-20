010ca98be8df3bed387a9e2fbc0f672d236509bd
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-20 20:32:08 +0000

    Fixed many typo in templates.

--- trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/src/main/java/user/EditController.java
+++ trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/src/main/java/user/EditController.java
@@ -37,15 +37,16 @@ public class EditController extends SimpleFormController implements UserConstant
 
     
 	@Override
-	protected void doSubmitAction(Object command) throws Exception {     
-        User user = (User) command;
+    protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command,
+    		BindException errors) throws Exception {
+    	User user = (User) command;
         logger.debug("Updating user info " + user);
         
         try {
             userDao.update(user);
             logger.info("User " + user.getUsername() + " updated.");
             
-            //Do we need to update loggin user?            
+            //Do we need to update loggin user?
             User loggedInUser = (User)ServletRequestHelper.getSession(request, userSessionKey);
             if(loggedInUser.getId() == user.getId() && !loggedInUser.getUsername().equals(user.getUsername())){
                 logger.info("Logged in username has changed. Updating user session object.");
@@ -56,12 +57,13 @@ public class EditController extends SimpleFormController implements UserConstant
             logger.error("Failed to edit user " + user, e);
             throw new ModelAndViewDefiningException(new ModelAndView("error", "message", e.getMessage()));
         }
+    	return super.onSubmit(request, response, command, errors);
     }
 
     
     @Override
     protected Object formBackingObject(HttpServletRequest request) throws Exception {
-        int id = ServletRequestHelper.getOptinalIntParameter(request, "id", 0);
+        int id = ServletRequestHelper.getOptionalParameter(request, "id", 0);
         User user = null;
         
         if(id >0){
