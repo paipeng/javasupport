0654ac9a5c18c0f9e69c08f21de97e6b175eba3d
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-20 14:08:59 +0000

    updated all archetype to specific version on script. latest one has problem!

--- trunk/maven-support/bin/create-javaapp.bat
+++ trunk/maven-support/bin/create-javaapp.bat
@@ -1,1 +1,1 @@
-mvn archetype:create -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal -DarchetypeGroupId=javasupport.maven -DarchetypeArtifactId=maven-archetype-javaapp -DarchetypeVersion=0.0.3-SNAPSHOT -DgroupId=%1 -DartifactId=%2
+mvn org.apache.maven.plugins:maven-archetype-plugin:1.0-alpha-7:create -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/internal -DarchetypeGroupId=javasupport.maven -DarchetypeArtifactId=maven-archetype-javaapp -DarchetypeVersion=0.0.3-SNAPSHOT -DgroupId=%1 -DartifactId=%2
