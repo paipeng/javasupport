c0917285a599cb7c6abd59ea2091eceaa766c0e6
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-03-31 21:19:31 +0000

    fixed scripts

--- trunk/maven-support/bin/create-springwebapp-hibernate.sh
+++ trunk/maven-support/bin/create-springwebapp-hibernate.sh
@@ -5,7 +5,6 @@ fi
 
 mvn org.apache.maven.plugins:maven-archetype-plugin:1.0-alpha-7:create \
   -DremoteRepositories=http://faxintelligence.com:8082/archiva/repository/snapshots \
-  -DremoteRepositories=http://bb:8082/archiva/repository/connextions \
   -DarchetypeGroupId=javasupport.maven \
   -DarchetypeArtifactId=maven-archetype-springwebapp-hibernate \
   -DarchetypeVersion=0.0.3-SNAPSHOT\
