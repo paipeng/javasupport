4e5c4e385c6627b289be82e0c157f29ba3289724
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-04-05 03:44:32 +0000

    Fixed view constants.

--- trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/src/main/java/user/ListController.java
+++ trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/src/main/java/user/ListController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 /** List and browse Controller for User. */
-public class ListController extends AbstractController {
+public class ListController extends AbstractController implements UserConstants {
 	private UserDao userDao;
 
 	public void setUserDao(UserDao that) {
@@ -19,6 +19,6 @@ public class ListController extends AbstractController {
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		List<User> userList = userDao.findAll();		
-		return new ModelAndView("member/list", "userList", userList);
+		return new ModelAndView(USER_LIST_VIEW, "userList", userList);
 	}
 }
