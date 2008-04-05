29c208263bf9fde091bc64db71415d36bcdc3c06
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-04-05 03:36:30 +0000

    fixed shell scritp

--- trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/bin/setup-db.sh
+++ trunk/maven-support/maven-archetype-springwebapp-hibernate/src/main/resources/archetype-resources/bin/setup-db.sh
@@ -1,4 +1,5 @@
-echo create database ${artifactId}_dev | mysql -u root
-echo grant all on ${artifactId}_dev.* to 'devuser'@'localhost' identified by 'devuser123' | mysql -u root
-echo flush privileges | mysql -u root
+#!/bin/sh
+echo "create database if not exists ${artifactId}_dev" | mysql -u root
+echo "grant all on ${artifactId}_dev.* to 'devuser'@'localhost' identified by 'devuser123'" | mysql -u root
+echo "flush privileges" | mysql -u root
 mvn hibernate3:hbm2ddl
