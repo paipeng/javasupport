c7971d0c16c2f5a974e1d24ba17c890330e05a61
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-06-03 23:45:24 +0000

    fixed scalawebapp

--- trunk/maven-support/maven-archetype-scalawebapp/src/main/resources/archetype-resources/src/main/scala/WelcomeServlet.scala
+++ trunk/maven-support/maven-archetype-scalawebapp/src/main/resources/archetype-resources/src/main/scala/WelcomeServlet.scala
@@ -2,15 +2,13 @@ package ${groupId}
 
 import java.io.IOException
 import java.util.Date
-
 import javax.servlet.ServletException
-import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
+import javax.servlet.http.{HttpServlet, HttpServletRequest=>Req, HttpServletResponse=>Resp}
 import org.slf4j.{Logger, LoggerFactory}
 	
 class WelcomeServlet extends HttpServlet {
 	val logger = LoggerFactory.getLogger(this.getClass)
-	
-	override def doGet(req:HttpServletRequest, resp:HttpServletResponse):Unit = {
+	override def doGet(req: Req, resp: Resp): Unit = {
 		logger.debug("Received request for message")
 		val serverTime = new Date()
 		req.setAttribute("message", "Hello guess, the current server time is: " + serverTime)
