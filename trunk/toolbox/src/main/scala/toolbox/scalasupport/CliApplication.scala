5f2f7382a2de44b7a1cd59dd45bd95776fce71e8
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-06-16 21:26:45 +0000

    Auto checking.

--- trunk/toolbox/src/main/scala/toolbox/scalasupport/CliApplication.scala
+++ trunk/toolbox/src/main/scala/toolbox/scalasupport/CliApplication.scala
@@ -2,19 +2,47 @@ package toolbox.scalasupport
 
 import scala.collection.mutable.{ HashMap, ListBuffer } 
 trait CliApplication {
-  def usage = "Usage: scala " + getClass.getName + " [options] [arg ...]" + """
-    |   -h display helpage.
-    |   -d turn debug mode ON.  
-    """.stripMargin
-  def printExitUsage {
-    println(usage)
-    exit(1)
+  /**
+   * Simple parser for short and long style command line options and arguments.
+   * Short option format is single dash prefix with single char flag. Anything beyong
+   * the first char will be treated as parameter value for it's flag.
+   * Long option format is two dashes prefix with one or more chars flag. Option parameter
+   * must specify with equal char.
+   *
+   * @return tuple of 2 elements: list of clean args and map of options.
+   */
+  def parseOptions(argv: Array[String]): (List[String], Map[String, String])={
+      var args = List[String]()
+      var opts = Map[String,String]()
+      for(arg <- argv){
+          if (arg.startsWith("--")) {
+              val s = arg.substring(2).split("=")
+              if (s.length >= 2) {
+                  opts = opts + (s(0) -> s(1))
+              } else {
+                  opts = opts + (s(0) -> "true")
+              }
+          } else if (arg.startsWith("-")) {
+              val s = arg.substring(1)
+              if (s.length() > 1) {
+                  opts = opts + (s.substring(0, 1) -> s.substring(1))
+              } else {
+                  opts = opts + (s -> "true")
+              }
+          } else {
+              args = arg :: args;
+          }
+      }
+      (args.reverse, opts);
+  } 
+  def exitWith(msg: String) {
+    RichSystem.exitWith(msg, 0)
   }
+  
   /** Parse commandline args and return immutable args list and options map. 
   We will automatically insert/define -h for help and -d for debug option
   if subclass does not provide them. Also if subclass not defined a match
   then it will be add to arg list by default.
-  */
   def parseOptions(args: Array[String])
   (f: (ListBuffer[String], HashMap[String, String]) => PartialFunction[String, Unit]):
   (List[String], Map[String, String]) = {
@@ -31,4 +59,5 @@ trait CliApplication {
       
     (argList.toList, Map(options.toArray: _*))
   }  
+  */
 }
