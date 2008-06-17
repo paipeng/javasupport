44766c755d4ec441b7111f67a1d2b359d7e5c9e2
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-06-17 04:13:14 +0000

    Auto checkin.

--- trunk/toolbox/src/main/scala/toolbox/config/Config.scala
+++ trunk/toolbox/src/main/scala/toolbox/config/Config.scala
@@ -1,8 +1,20 @@
 package toolbox.config
 object ConfigDemo extends Application {
-  
+  println(Config.props)
+  //println(toolbox.scalasupport.RichSystem.props)
 }
 
-object Config{
-  
+object Config {
+  val props = PropertiesFile("config.properties")  
+}
+
+import scala.collection.jcl.MapWrapper
+object PropertiesFile {
+  def apply(name: String): MapWrapper[String, String] = {
+    val props = new java.util.Properties
+    props.load(new java.io.FileReader(name))
+    new MapWrapper[String, String]{
+      def underlying = props.asInstanceOf[java.util.Map[String,String]]
+    }
+  }
 }
