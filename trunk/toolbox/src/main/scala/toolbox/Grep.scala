76476c3afa7fbcd6ea21012f278cfc2ed5b674d2
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-06-17 02:35:51 +0000

    Auto checking.

--- trunk/toolbox/src/main/scala/toolbox/Grep.scala
+++ trunk/toolbox/src/main/scala/toolbox/Grep.scala
@@ -5,29 +5,41 @@ object Grep extends CliApplication{
   def main(argv: Array[String]):Unit = {    
     val (args, opts) = parseOptions(argv)
     
-    if(opts.contains("h") || opts.contains("help")){
+    if(opts.contains("h")){
       exitWith(
       """Grep text inside a text files.
         | usage: Grep [options] text file1 [file2...]
         | usage: Grep [options] text dir [dir2...]
-        | [options] --help, -h   Display helpage.
-        | [options] --debug      Debug search
-      """)
+        | [options] -h   Display helpage.
+        | [options] -v   Revese display with found text.
+        | [options] -s   Read from STDIN
+        | [options] -d   Debug search.
+      """.stripMargin)
     }
     
-    val debug = opts.getOrElse("debug", "false").toBoolean
-    val (search::files) = args    
+    val debug = opts.contains("d")
+    val reverse = opts.contains("v")
+    val stdin = opts.contains("s")
+    val (search::files) = args
+    
     def grep(file: java.io.File){
       if(file.isDirectory) 
         file.listFiles.map( grep(_) )
       else
         if(debug) println("Searching " + file.getPathname)
-        for(ln <- scala.io.Source.fromFile(file).getLines){
-          if(ln.contains(search)) print(file.getPathname+":"+ln)  
-        }
+        if(reverse)          
+          for(ln <- scala.io.Source.fromFile(file).getLines)
+            if(!ln.contains(search)) print(file.getPathname+":"+ln)            
+        else
+          for(ln <- scala.io.Source.fromFile(file).getLines)
+            if(ln.contains(search)) print(file.getPathname+":"+ln)  
     }
-    for(fn <- files){
-      grep(new java.io.File(fn))  
+    if(stdin){
+      for(ln <- scala.io.Source.fromInputStream(System.in).getLines)
+       if(!ln.contains(search)) print(ln)             
+    }else{
+      for(fn <- files)
+        grep(new java.io.File(fn))  
     }
   }
 }
