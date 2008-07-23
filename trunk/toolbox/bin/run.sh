c1a5e8902e6969574d8125259d7704b568458d5e
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2008-07-23 20:27:50 +0000

    fixed run script to support java5.

--- trunk/toolbox/bin/run.sh
+++ trunk/toolbox/bin/run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 SCRIPT_HOME=`dirname $0`
 SCRIPT_PARENT_HOME=`cd $SCRIPT_HOME/.. && pwd`
-java $JAVA_OPTS -classpath "$JAVA_CLASSPATH:$SCRIPT_PARENT_HOME/lib/*" $@
+
+# this is for java6 only.
+# java $JAVA_OPTS -classpath "$JAVA_CLASSPATH:$SCRIPT_PARENT_HOME/lib/*" $@
+
+# for java5, add all jar files from lib
+for file in $SCRIPT_PARENT_HOME/lib/*.jar; do
+  if [[ -f $file ]]; then CP=$CP:$file; fi
+done
+
+java $JAVA_OPTS -classpath "$JAVA_CLASSPATH:$CP" $@
+
