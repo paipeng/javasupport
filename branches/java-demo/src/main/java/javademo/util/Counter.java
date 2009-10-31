ca3f4bbea4a49d50bb248b5bc752114bd41d6b75
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2009-10-31 01:11:35 +0000

    init proj setup.

---
+++ branches/java-demo/src/main/java/javademo/util/Counter.java
@@ -0,0 +1,8 @@
+//Counter for inner class
+package javademo.util;
+
+public class Counter {
+    private int count = 0;
+    public void inc() { count ++; }
+    public int getCount() { return count; }
+}
