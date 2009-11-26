32c0573ddfa1493f1c8f3568d288799824b58786
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2009-11-26 04:03:59 +0000

    Added empty class QuickQueueSpeedTest.

--- branches/hornetq-examples/src/main/java/deng/hornetqexamples/core/EmbeddedServer.java
+++ branches/hornetq-examples/src/main/java/deng/hornetqexamples/core/EmbeddedServer.java
@@ -9,6 +9,9 @@ import org.hornetq.jms.server.impl.JMSServerManagerImpl;
 /**
  * This example is based from blog entry here:
  * http://hornetq.blogspot.com/2009/09/hornetq-simple-example-using-maven.html
+ *
+ * To run this in maven:
+ *   vn exec:java -Dexec.mainClass=deng.hornetqexamples.core.EmbeddedServer -Dexec.args="file:///hq/config/stand-alone/non-clustered"
  * 
  * @author zemian
  * 
@@ -16,6 +19,7 @@ import org.hornetq.jms.server.impl.JMSServerManagerImpl;
 public class EmbeddedServer {
 	public static void main(String[] args) throws Exception {
 		String configDir = args[0];
+		System.out.println("configDir (Needs to be URL format!): " + configDir);
 		try {
 			FileConfiguration configuration = new FileConfiguration();
 			configuration.setConfigurationUrl(configDir + "/hornetq-configuration.xml");
