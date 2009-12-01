480621fd097ef4f5b35f403c135be347a756d139
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2009-12-01 11:00:01 +0000

    renamed projects, added simple-spring-app. modified archetype.

---
+++ branches/spring-projects/simple-spring-app/src/main/java/deng/simplespringapp/LoggerDemo.java
@@ -0,0 +1,28 @@
+package deng.simplespringapp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Demo of using a Logger.
+ * 
+ * @author Zemian Deng
+ *
+ * Created on Nov 12, 2009
+ */
+public class LoggerDemo {
+	private Log logger = LogFactory.getLog(getClass());
+	
+	public static void main(String[] args) {
+	  new LoggerDemo().logAllLevels();
+	}
+	
+	public void logAllLevels() { 
+		logger.trace("A message from logger.");
+		logger.debug("A message from logger.");
+		logger.info("A message from logger.");
+		logger.warn("A message from logger.");
+		logger.error("A message from logger.");
+		logger.fatal("A message from logger.");
+	}
+}
