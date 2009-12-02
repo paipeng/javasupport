bef07a44527980a189a0631e49d90fe7eae2b6ad
thebugslayer <thebugslayer@c16894b4-1c48-0410-bf92-a11f6cf122dc>
2009-12-02 18:42:37 +0000

    update ClassLoaderDemo.

--- branches/java-examples/src/main/java/deng/javaexamples/ClassLoaderDemo.java
+++ branches/java-examples/src/main/java/deng/javaexamples/ClassLoaderDemo.java
@@ -1,10 +1,89 @@
 package deng.javaexamples;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
+
+/**
+ * 
+
+* For more examples, checkout Groovy CL
+	println(this.class.classLoader.parent.parent.parent)
+	println(this.class.classLoader.rootLoader)
+	println(Thread.currentThread().getContextClassLoader())
+	
+	OUTPUT:
+	sun.misc.Launcher$AppClassLoader@11b86e7
+	org.codehaus.groovy.tools.RootLoader@42e816
+	org.codehaus.groovy.tools.RootLoader@42e816
+
+
+* JDK provided class loader.
+java.lang.Object
+  extended by java.lang.ClassLoader
+      extended by java.security.SecureClassLoader
+          extended by java.net.URLClassLoader
+              extended by javax.management.loading.MLet
+                  extended by javax.management.loading.PrivateMLet
+
+ 
+ * @author dengz1
+ *
+ */
 public class ClassLoaderDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ClassLoaderDemo demo = new ClassLoaderDemo();
-		demo.arrayCL();
+		//demo.objCL();
+		//demo.threadContextCL();
+		demo.loadExternalClass();
+	}
+
+	private void loadExternalClass() throws Exception {
+//		//Test 1
+//		try {
+//			Class<?> cls = Class.forName("deng.cltest.CLTest", true, null);
+//			System.out.println(cls);
+//		} catch (ClassNotFoundException e) {
+//			System.out.println("Expected.");
+//		}
+		
+//		//Test 2
+//		String jarfile = "file:///D:/tmp/cl.jar";
+//		ClassLoader cl = new URLClassLoader(new URL[]{new URL(jarfile) });
+//		Class<?> cls = Class.forName("deng.cltest.CLTest", true, cl);
+//		System.out.println(cls); // SUCCESS!
+		
+		//Test 3
+		// This will not find class! bc it will use this.getClass().getClassLoader() instead!
+		// So null to #forName classloader will not automatically use Thread.getContextClassLoader()!
+		// usage for that has to be explicit.
+		// So this is a built-in "ThreadLocal" space for this particular object!
+		String jarfile = "file:///D:/tmp/cl.jar";
+		ClassLoader cl = new URLClassLoader(new URL[]{new URL(jarfile) });
+		Thread thread = Thread.currentThread();
+		ClassLoader currentCL = thread.getContextClassLoader();
+		thread.setContextClassLoader(cl);
+		Class<?> cls = Class.forName("deng.cltest.CLTest", true, null);
+		System.out.println(cls);
+		thread.setContextClassLoader(currentCL);
+	}
+
+	public void threadContextCL() {
+		Thread thread = Thread.currentThread();
+		ClassLoader cl = thread.getContextClassLoader();		
+		showCL("* Thread Context ClassLoader", cl);
+		
+		cl = ClassLoader.getSystemClassLoader();
+		showCL("* ClassLoader.getSystemClassLoader()", cl);
+	}
+	
+	private void showCL(String subj, ClassLoader cl) {
+		System.out.println(subj);
+		System.out.println(cl);
+		while ((cl = cl.getParent()) != null) {
+			System.out.println("parent: " + cl);
+		}
 	}
 
 	/**
